58e4b2f528d07bd7ea610b36b4e456399677d0f0
vistritpandey <vistritpandey@gmail.com>
2020-09-29 17:55:51 +0530

    u

--- script.js
+++ script.js
@@ -8,26 +8,26 @@ const quizData = [
         correct: "d",
     },
     {
-        question: "Who is the President of US?",
-        a: "Florin Pop",
-        b: "Donald Trump",
-        c: "Ivan Saldano",
-        d: "Mihai Andrei",
+        question: "Who is the Prime Minister of India?",
+        a: "Manmohan Singh",
+        b: "Narendra Modi",
+        c: "Amit Shah",
+        d: "Rahul Gandhi",
         correct: "b",
     },
     {
-        question: "What does HTML stand for?",
-        a: "Hypertext Markup Language",
-        b: "Cascading Style Sheet",
-        c: "Jason Object Notation",
-        d: "Helicopters Terminals Motorboats Lamborginis",
+        question: "Who is the CM of UP?",
+        a: "Yogi Adityanath",
+        b: "Akhilesh Yadav",
+        c: "Mayawati",
+        d: "Mulayam Singh Yadav",
         correct: "a",
     },
     {
-        question: "What year was JavaScript launched?",
-        a: "1996",
-        b: "1995",
-        c: "1994",
+        question: "What year was MOM(Mars Orbital Mission) launched?",
+        a: "2010",
+        b: "2016",
+        c: "2019",
         d: "none of the above",
         correct: "b",
     },
@@ -46,12 +46,9 @@ let currentQuiz = 0;
 let score = 0;
 
 loadQuiz();
-
 function loadQuiz() {
     deselectAnswers();
-
     const currentQuizData = quizData[currentQuiz];
-
     questionEl.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
@@ -61,38 +58,30 @@ function loadQuiz() {
 
 function getSelected() {
     let answer = undefined;
-
     answerEls.forEach((answerEl) => {
         if (answerEl.checked) {
             answer = answerEl.id;
         }
     });
-
     return answer;
 }
-
 function deselectAnswers() {
     answerEls.forEach((answerEl) => {
         answerEl.checked = false;
     });
 }
-
 submitBtn.addEventListener("click", () => {
-    // check to see the answer
     const answer = getSelected();
-
     if (answer) {
         if (answer === quizData[currentQuiz].correct) {
             score++;
         }
-
         currentQuiz++;
         if (currentQuiz < quizData.length) {
             loadQuiz();
         } else {
             quiz.innerHTML = `
-                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
-                
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>             
                 <button onclick="location.reload()">Reload</button>
             `;
         }
